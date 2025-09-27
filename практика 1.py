8fcc364fdeaccd07b4d9e0d89e2454ff5e456db4
Denis <serg2004q@mail.ru>
2025-09-27 12:11:15 +1000

    version 1.0

---
+++ практика 1.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+PATH_TO_FILE: str = '/content/Coffe_sales.csv'
+df = pd.read_csv(PATH_TO_FILE)
+df
+df.head()
+df.info()
+df.describe()
+print(df.dtypes)
+for column in df.columns:
+    print(f'{column}: {df[column].nunique()} уникальных значений')
+sns.histplot(df['alcohol'], kde=True)
+plt.title('Распределение Alcohol')
+plt.xlabel('Alcohol')
+plt.ylabel('Частота')
+plt.show()
+sns.countplot(x='quality', data=df)
+plt.title('Распределение quality')
+plt.xlabel('quality')
+plt.ylabel('Количество')
+plt.show()
+sns.boxplot(x=df['chlorides'])
+plt.title('Boxplot chlorides')
+plt.xlabel('chlorides')
+plt.show()
+# Корреляционная матрица
+correlation_matrix = df.corr()
+
+# Визуализация тепловой карты
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+plt.title('Корреляционная матрица')
+plt.show()
+
+# готово
